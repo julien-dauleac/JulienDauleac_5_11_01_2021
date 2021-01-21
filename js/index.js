9eefd772c44ce5728c56700c3c61d5d3707c4604
Julien Dauleac <julien.dauleac@gmail.com>
2021-01-21 17:50:16 +0100

    Setting up the first page: home

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ function populateTableList() {
     products.forEach(prod =>
         listOfProducts += `
         <tr class="text-center">
-        <td><img src=${prod.picture} alt="" class="img-fluide img-thumbnail w-50"></td>
+        <td><img src=${prod.picture} alt="" class="img-fluide img-thumbnail"></td>
         <td class="w-25 align-middle">${prod.name}</td>
         <td class="w-25 align-middle">${prod.price}€</td>
         <td class="w-25 align-middle"><button class="btn btn-info">Voir plus</button></td>
