3e61c9ebe14f9e922d78751d3926a5f79fe1a4f0
Julien Dauleac <julien.dauleac@gmail.com>
2021-01-25 16:40:40 +0100

    Editing tables in index and product pages

--- js/index.js
+++ js/index.js
@@ -8,17 +8,18 @@ request.onreadystatechange = function() {
 request.open("GET", "http://localhost:3000/api/cameras");
 request.send();
 
-function Product(picture,name,price){
+function Product(picture,name,price, id){
     this.picture = picture;
     this.name = name;
     this.price = price;
+    this.id = id;
 }
 let products = [];
- const productOne = new Product('http://localhost:3000/images/vcam_1.jpg','Zurss 50S',49900);
- const productTwo = new Product('http://localhost:3000/images/vcam_2.jpg','Hirsch 400DTS',309900);
- const productThree = new Product('http://localhost:3000/images/vcam_3.jpg','Franck JS 105',209900);
- const productFour = new Product('http://localhost:3000/images/vcam_4.jpg','Kuros TTS',159900);
- const productFive = new Product('http://localhost:3000/images/vcam_5.jpg','Katatone',59900);
+ const productOne = new Product('http://localhost:3000/images/vcam_1.jpg','Zurss 50S',49900,'5be1ed3f1c9d44000030b061');
+ const productTwo = new Product('http://localhost:3000/images/vcam_2.jpg','Hirsch 400DTS',309900,"5be1ef211c9d44000030b062");
+ const productThree = new Product('http://localhost:3000/images/vcam_3.jpg','Franck JS 105',209900,"5be9bc241c9d440000a730e7");
+ const productFour = new Product('http://localhost:3000/images/vcam_4.jpg','Kuros TTS',159900,"5be9c4471c9d440000a730e8");
+ const productFive = new Product('http://localhost:3000/images/vcam_5.jpg','Katatone',59900,"5be9c4c71c9d440000a730e9");
 products.push(productOne,productTwo,productThree,productFour,productFive);
 
 function populateTableList() {
@@ -29,8 +30,10 @@ function populateTableList() {
         <td><img src=${prod.picture} alt="" class="img-fluide img-thumbnail"></td>
         <td class="w-25 align-middle">${prod.name}</td>
         <td class="w-25 align-middle">${prod.price}€</td>
-        <td class="w-25 align-middle"><button class="btn btn-info">Plus de détails</button></td>
+        <td class="w-25 align-middle"><a href="produit.html?id=${prod.id}" class="btn btn-info">Details</a></td>
         `
     )
     document.getElementById('productList').innerHTML = listOfProducts
 }
+
+populateTableList()
--- js/produit.js
+++ js/produit.js
@@ -1,27 +1,13 @@
-function Product(picture, name, price, description, lenses){
+function Product(picture, name, price, description, lenses, amount){
     this.picture = picture;
     this.price = price;
     this.name = name;
     this.description = description;
     this.lenses = lenses;
+    this.amount = amount;
 }
 let productDescription = [];
-const productOneDescription = new Product('http://localhost:3000/images/vcam_1.jpg','Zurss 50S',49900,
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    ['35mm 1.4, 50mm 1.6']);
-const productTwoDescription = new Product('http://localhost:3000/images/vcam_2.jpg','Hirsch 400DTS',309900,
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    ['50mm 1.8", 60mm 2.8", 24-60mm 2.8/4.5']);
-const productThreeDescription = new Product('http://localhost:3000/images/vcam_3.jpg','Franck JS 105',209900,
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    ['25mm 4.5']);
-const productFourDescription = new Product('http://localhost:3000/images/vcam_4.jpg','Kuros TTS',159900,
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    ['50mm 1.7", 35mm 1.4']);
-const productFiveDescription = new Product('http://localhost:3000/images/vcam_5.jpg','Katatone',59900,
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    ['50mm 1.4", 35mm 1.8", 28-200mm 2.8/4.5']);
-productDescription.push(productOneDescription,productTwoDescription,productThreeDescription,productFourDescription,productFiveDescription);
+productDescription.push(Product);
 
 function populateTableList() {
     let ProductOfDescription = '';
@@ -32,16 +18,38 @@ function populateTableList() {
         <td class="w-20 align-middle">${prod.name}</td>
         <td class="w-15 align-middle">${prod.price}€</td>
         <td class="w-25 align-middle">${prod.description}</td>
-        <td class="w-15 align-middle">${prod.lenses}</td>
-        <td class="w-15 align-middle"><button class="btn btn-info">Validé</button></td>
+        <td class="w-15 align-middle"><form name="lenses">
+                    <label>
+                        <select name="list" onChange="Lien()">
+                            <option value="#">Choisir une option de lentille
+                            <option value="#">${prod.lenses}
+                            <option value="#">${prod.lenses}
+                            <option value="#">${prod.lenses}
+                        </select>
+                    </label>
+                </form></td>
+        <td class="w-25 align-middle"><form name="amount">
+                    <label>
+                        <select name="list" onChange="Lien()">
+                            <option value="#">Choisir la quantité
+                            <option value="#">1
+                            <option value="#">2
+                            <option value="#">3
+                            <option value="#">4
+                            <option value="#">5
+                            
+                        </select>
+                    </label>
+                </form></td>
+        <td class="w-15 align-middle"><a href="panier.html" class="btn btn-info">Validé</a></td>
         `
     )
     document.getElementById('productOfDescription').innerHTML = ProductOfDescription
 }
 
-function Lien() {
-    i = document.Choix.Liste.selectedIndex;
-    if (i === 0) return;
-    url = document.Choix.Liste.options[i].value;
-    parent.location.href = url;
-}
+let urlSearchParams = URL.searchParams;
+
+let params = (new URL(document.location)).searchParams;
+let id = params.get('id');
+
+populateTableList()
