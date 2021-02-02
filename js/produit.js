b23881745d6f40d012bc4a3ce826e58287092c6e
Julien Dauleac <julien.dauleac@gmail.com>
2021-02-02 15:58:02 +0100

    Editing the product.html page and the product.js file

--- js/produit.js
+++ js/produit.js
@@ -5,38 +5,39 @@ function Product(picture, name, price, description, lenses){
     this.description = description;
     this.lenses = lenses;
 }
-let details = [];
+let products = [];
 let request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         let camera = JSON.parse(this.responseText);
         let params = new URLSearchParams(window.location.search);
         let id = params.get('id');
-        console.log(id)
+        let idSelection = camera.find((element) => element._id === id);
+        console.log(idSelection)
         let productCamera = new Product (camera.imageUrl, camera.name, camera.price, camera.description, camera.lenses)
-        details.push(productCamera);
-        detailsProduct()
+        products.push(productCamera);
+        populateTableList()
     }
 };
 request.open("GET", 'http://localhost:3000/api/cameras/');
 request.send();
 
-function detailsProduct() {
+function populateTableList() {
     let productOfDescription = '';
-    details.forEach(prod =>
+    products.forEach(camera =>
         productOfDescription += `
         <tr class="text-center">
-        <td><img src=${prod.picture} alt="" class="img-fluide img-thumbnail"></td>
-        <td class="w-20 align-middle">${prod.name}</td>
-        <td class="w-15 align-middle">${prod.price/100}€</td>
-        <td class="w-25 align-middle">${prod.description}</td>
+        <td><img src=${camera.picture} alt="" class="img-fluide img-thumbnail"></td>
+        <td class="w-20 align-middle">${camera.name}</td>
+        <td class="w-15 align-middle">${camera.price/100}€</td>
+        <td class="w-25 align-middle">${camera.description}</td>
         <td class="w-15 align-middle"><form name="lenses">
                     <label>
                         <select name="list" onChange="Lien()">
                             <option value="#">Choisir une option de lentille
-                            <option value="#">${prod.lenses}
-                            <option value="#">${prod.lenses}
-                            <option value="#">${prod.lenses}
+                            <option value="#">${camera.lenses}
+                            <option value="#">${camera.lenses}
+                            <option value="#">${camera.lenses}
                         </select>
                     </label>
                 </form></td>
