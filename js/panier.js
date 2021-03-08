f02b8c5439bd8392c9bbd45aa683c8533b1a505c
Julien Dauleac <julien.dauleac@gmail.com>
2021-03-08 16:21:01 +0100

    Setting up the button to empty the basket and the total of the basket

--- js/panier.js
+++ js/panier.js
@@ -39,23 +39,53 @@ function basketList() {
 let delete_btn = document.querySelectorAll(".delete");
 
 for (let l = 0; l < delete_btn.length; l++){
-    delete_btn[l].addEventListener("click" , (event) =>{
+    delete_btn[l].addEventListener('click' , (event) =>{
         event.preventDefault();
-    let productSelectionDelete = sendLocalStorage[l].lenses;
-    sendLocalStorage = sendLocalStorage.filter(el => el.lenses !== productSelectionDelete);
+    let productSelectionDelete = sendLocalStorage[l];
+    sendLocalStorage = sendLocalStorage.filter(el => el.lenses !== productSelectionDelete.lenses || el._id !== productSelectionDelete._id);
     localStorage.setItem("product", JSON.stringify(sendLocalStorage));
     alert("Ce produit a bien été supprimer du panier");
     window.location.href = "panier.html";
     })
 }
 
-// Tableau du total du panier //
+// Bouton pour vider le panier //
 
+let btnDeleteBasket = document.querySelector(".btn_delete_basket");
+btnDeleteBasket.innerHTML = `
+<td class="w-20 align-middle"><a class="btn btn-info delete_basket">Vider le panier</a></td>
+`;
 
+// Suppression de la key produit dans le local storage pour vider le panier //
+
+btnDeleteBasket.addEventListener('click', (e)=>{
+    e.preventDefault();
+    // Méthode .removeItem //
+    localStorage.removeItem('product');
+    alert("Le panier a bien été vidé");
+    window.location.href = "panier.html";
+});
 
 // Fonction du prix total du panier //
 
+let totalBasket = [];
+for (let n = 0; n < sendLocalStorage.length; n++){
+    let priceBasket = sendLocalStorage[n].price * sendLocalStorage[n].quantity;
+    totalBasket.push(priceBasket)
+}
 
+// Addition des prix des articles du panier avec la méthode .reducer //
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+const totalBaskets = totalBasket.reduce(reducer,0);
+console.log(totalBaskets);
+
+// Code HTML du "afficher total" //
+
+let priceHtml = document.querySelector(".cart_price");
+priceHtml.innerHTML = `
+ <td class ="w-25 align-middle"> Le prix total est de : ${totalBaskets} € </td>
+`;
 
 // Tableau de renvoi API //
 
@@ -68,7 +98,7 @@ let eMail = document.getElementById("email");
 let _idBasket = document.querySelector("#allBasket");
 for (let m = 0; m < _idBasket.length; m++){
 
-} // Parcourir le panier pour écrire les _id //
+} // Parcourir le panier pour les _id //
 
 // Evenement de soumission ( addeventlistener (onsubmit)) fetch post /order //
 
@@ -86,14 +116,13 @@ for (let m = 0; m < _idBasket.length; m++){
             city: ville.value,
             emailAddress: eMail.value
         }
-        let table =
-            {
+        let table = {
                 contacts: contact,
                 products: _idBasket
             }
         fetch('http://localhost:3000/api/cameras/order', {
             method: "POST",
-            body: JSON.stringify(table)
+            body: JSON.stringify({table})
         }).then(response => response.json()).then(json => console.log(json))
     if (form.checkValidity() === false) {
     event.stopPropagation();
